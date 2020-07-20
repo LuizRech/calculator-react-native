d16a73f01ce9f207d3ab8c6e380e8cff878d20a5
Luiz Rech <luizadre@hotmail.com>
2020-07-20 14:23:30 -0300

    Static Visual

--- App.js
+++ App.js
@@ -1,21 +1,85 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 export default function App() {
+
+    const characters = 
+    [
+      "AC", "DEL", "%", "/", 
+      7, 8, 9, "*", 
+      4, 5, 6, "-", 
+      1, 2, 3, "+", 
+      "+/-", 0, ".", "="
+    ];
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <>
+      <View style={styles.resultContainer}>
+        <Text style={styles.textCont}>
+          1 + 1 =
+        </Text>
+        <Text style={styles.textResult}>
+          2
+        </Text>
+      </View>
+    
+      <View style={styles.padContainer}>
+        
+          {
+            characters.map((char) => (
+              char == "=" ?
+               <TouchableOpacity key={char} style={[styles.characters, {backgroundColor: '#c8e6c1'}]}>
+                  <Text style={styles.charactersN}>{char}</Text>
+                </TouchableOpacity>
+              :
+                <TouchableOpacity key={char} style={[styles.characters, {backgroundColor: typeof(char) === 'number' ? 'white' : '#e6e6e6'}]}>
+                  <Text style={styles.charactersN}>{char}</Text>
+                </TouchableOpacity>
+
+            ))
+          }
+
+      </View>
+    </>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  resultContainer: {
+    minHeight: 300,
+    width: "100%",
+    backgroundColor: "#f5f5f5",
+    justifyContent: "flex-end"
   },
+
+  textCont: {
+    fontSize: 25,
+    alignSelf: "flex-end",
+    paddingRight: 15
+  },
+
+  textResult: {
+    fontSize: 55,
+    alignSelf: "flex-end",
+    paddingRight: 15
+  },
+
+  padContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap'
+  },
+  
+  characters: {
+    minHeight: 90,
+    minWidth: 90,
+    alignItems: "center",
+    justifyContent: "center",
+    flex: 2,
+    borderWidth: 0.5
+  }, 
+  
+  charactersN: {
+    fontSize: 22
+  }
 });
