60a0d8c276f1ad1e66eb1de68af4909f7517af0d
Luiz Rech <luizadre@hotmail.com>
2020-07-22 20:24:08 -0300

    calculator

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, Alert } from 'react-native';
 
 export default function App() {
 
+    const [ lastNumber, setLastNumber ] = useState();
+    const [ currentNumber, setCurrentNumber ] = useState('');
+ 
     const characters = 
     [
       "AC", "DEL", "%", "/", 
@@ -13,14 +16,75 @@ export default function App() {
       "+/-", 0, ".", "="
     ];
 
+    function handleButtonPress(buttonPressed){
+      if(buttonPressed == "+" || buttonPressed == "-" || buttonPressed == "*" || buttonPressed == "/"){
+        setCurrentNumber(currentNumber + " " + buttonPressed + " ");
+        return;
+      }
+
+      switch(buttonPressed){
+        case 'AC':
+          setLastNumber('');
+          setCurrentNumber('');
+        return;
+        case 'DEL':
+          setLastNumber('1');
+          setCurrentNumber('1');
+        return;
+        case '=':
+          setLastNumber(currentNumber + "=");
+          calculate()
+        return;
+      }
+
+     setCurrentNumber(currentNumber + buttonPressed);
+    }
+
+    function calculate(){
+      
+     const splitNumbers = currentNumber.split(" ");
+     const firstNumber = parseFloat(splitNumbers[0]);
+     const secondNumber = parseFloat(splitNumbers[2]);
+     const operation = splitNumbers[1];
+
+      if(!isNaN(secondNumber)){
+        switch(operation){
+          case '+':
+            var result = firstNumber + secondNumber;
+            setCurrentNumber(result);
+          return;
+          case '-':
+            var result = firstNumber - secondNumber;
+            setCurrentNumber(result);
+          return;
+          case '*':
+            var result = firstNumber * secondNumber;
+            setCurrentNumber(result);
+          return;
+          case '/':
+            var result = firstNumber / secondNumber;
+            setCurrentNumber(result);
+          return;
+          default: 
+            setLastNumber('');
+            setCurrentNumber('');
+          return;
+        }
+      }else{
+        Alert.alert("Invalid format");
+        setLastNumber('');
+        setCurrentNumber('');
+      }
+    }
+
   return (
     <>
       <View style={styles.resultContainer}>
         <Text style={styles.textCont}>
-          1 + 1 =
+          {lastNumber}
         </Text>
         <Text style={styles.textResult}>
-          2
+          {currentNumber}
         </Text>
       </View>
     
@@ -29,11 +93,19 @@ export default function App() {
           {
             characters.map((char) => (
               char == "=" ?
-               <TouchableOpacity key={char} style={[styles.characters, {backgroundColor: '#c8e6c1'}]}>
+                <TouchableOpacity
+                  key={char} 
+                  style={[styles.characters, {backgroundColor: '#c8e6c1'}]}
+                  onPress={() => handleButtonPress(char)}
+                >
                   <Text style={styles.charactersN}>{char}</Text>
                 </TouchableOpacity>
               :
-                <TouchableOpacity key={char} style={[styles.characters, {backgroundColor: typeof(char) === 'number' ? 'white' : '#e6e6e6'}]}>
+                <TouchableOpacity 
+                  key={char} 
+                  style={[styles.characters, {backgroundColor: typeof(char) === 'number' ? 'white' : '#e6e6e6'}]}
+                  onPress={() => handleButtonPress(char)}
+                >
                   <Text style={styles.charactersN}>{char}</Text>
                 </TouchableOpacity>
 
